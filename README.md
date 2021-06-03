286fce773499988f8d374ed187319705cf2271d2
Roxanne <>
2021-06-03 20:29:57 +0000

    Final Touches

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# Rock, Paper, Scissors
+ - Objective: to program and design a five round game of rock paper scissors.
+- Time-frame:
+~ 20 hrs (CSS:5, JS:15).
+
+### Learning points:
+- Programming buttons with event handlers.
+- DOM modification methods to connect the GUI with the program of the game.
+- Efficient development and utilization of helper functions.
+
+### Demo 👉https://rock-paper-scissors.roxannecodes.repl.co/
+
+This Project is part of The Odin Project curriculum: https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/rock-paper-scissors
--- script.js
+++ script.js
@@ -1,53 +1,54 @@
 
-//Handles the click event after user selects a move
+//Handles the click event after user selects a move.
 document.getElementById("rock").onclick=playRock;
 document.getElementById("paper").onclick=playPaper;
 document.getElementById("scissors").onclick= playScissors;
  
-//Functions that utilize the users first move to initialize the game (and also display user's choice)
+//Functions that utilize the users first move to initialize the game (and also display user's choice).
 let uc=document.getElementById("user-choice");
 
 function playRock (){
-  uc.innerHTML= "You chose: Rock";
+  //uc.innerHTML= "You: Rock";
   playRound("rock");  
 }
 
 function playPaper (){
-  uc.innerHTML= "You chose: Paper";
+  //uc.innerHTML= "You: Paper";
   playRound ("paper");
 }
 
 function playScissors (){
-  uc.innerHTML= "You chose: Scissors";
+  //uc.innerHTML= "You: Scissors";
   playRound ("scissors");
 }
 
-//Function that gets computer's random choice (and also displays it)
 let cc =document.getElementById("computer-choice");
 let choices = ["rock", "paper", "scissors"];
 
+//Function that gets computer's random choice (and also displays it).
 function computerPlay (choices){
  let randomIndex = Math.floor (Math.random() * 3);
  let randomChoice = choices [randomIndex];
- cc.innerHTML= "Computer chose: " + randomChoice;
+ //cc.innerHTML= "Computer: " + randomChoice;
  return randomChoice;
 }
 
-//Main game function -- displays results and returns both total scores after every round 
-//cache the DOM and reset scores 
+//cache the DOM and reset scores.
 let us= document.getElementById("user-score");
 let cs=document.getElementById("computer-score");
 let results=document.getElementById("sub-title");
 let userScore =0;
 let compScore=0;
 
+// ** Main game function ** displays results and returns both total scores after every round.
 function playRound (userChoice){
 let userSelect=userChoice;
 let compSelect=computerPlay(choices);
 
-//since best out of five wins...
+// ** since best out of five wins the GAME, allows plays till one wins.
 if (userScore<5 && compScore<5){  
 
+// ** all of the possible scenarios for single ROUND:
  if (userSelect ===compSelect){
  results.innerHTML= "This round is a tie 🙃";
  cs.innerHTML= "Computer's Score: "+ compScore;
@@ -61,7 +62,7 @@ if (userScore<5 && compScore<5){
  }
   else if (userSelect==="rock" && compSelect==="scissors"){
   userScore +=1;
-  restults.innerHTML= "You won: rock beats scissors.😄";
+  results.innerHTML= "You won: rock beats scissors.😄";
   cs.innerHTML= "Computer's Score: "+ compScore;
   us.innerHTML= "Your Score: "+ userScore;
  }
@@ -79,24 +80,27 @@ if (userScore<5 && compScore<5){
   return userScore;
   return compScore; 
 }
+//if either player has won:
 else if (userScore==5){
   results.innerHTML= "Game Over: YOU WON!😄";
   cs.innerHTML= "Computer's Score: "+ compScore;
   us.innerHTML= "Your Score: "+ userScore;
+  //end game when user wins
+  alert("Game over: press f5 to play again");
 }
 else if (compScore===5){
  results.innerHTML= "Game Over: You lost.😿";
  us.innerHTML= "Your Score: "+ userScore;
  cs.innerHTML= "Computer's Score: "+ compScore;
-}else{
-  results.innerHTML = " *** PROGRAM ERROR*** ";
+ //end game when comp wins
+ alert("Game over: press f5 to play again");
+ }
+   
 }
-}
 
 
 
 
-//playRound (userChoice);
 
 
 
