467d31c6ee57135acbd32ed5870ea62ecabf04a6
Roxanne <roxanne.shoar@gmail.com>
2021-06-25 18:40:40 +0000

    Update user refresh prompt

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ else if (compScore===5){
  us.innerHTML= "Your Score: "+ userScore;
  cs.innerHTML= "Computer's Score: "+ compScore;
  //end game when comp wins
- alert("Game Over: Press F5 or refresh browser to play again");
+ alert("Game Over: Refresh your browser to play again 🙃.");
  }
    
 }
